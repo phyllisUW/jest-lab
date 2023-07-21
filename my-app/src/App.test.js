039ee72bc7ac0f9359dfbef93e6866840476e39c
phyllisUW <phychen@uw.edu>
2023-07-20 18:08:33 -0700

    added jest tests

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event"
+
+// Demo test from class 
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // first test
+  // screen.debug();
+
+    //Checking if checkbox is checked
+  userEvent.click(screen.getByTestId("checkbox"));
+    //Checking if checkbox is not checked
+  expect(screen.getByTestId("checkbox")).toBeChecked()
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
+
+
+// my tests 
+test("testing for hello world in the page", () => {
+  render(<App />);
+  screen.debug();
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
+});
+
+test("testing for if the button is clickable", () => {
+  render(<App />);
+  screen.debug();
+  const button = screen.getByRole('button');
+  userEvent.click(button);
 });
